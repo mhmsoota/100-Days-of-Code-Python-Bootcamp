d468e3fc1db1a67bf1933287e2852ee684a66fb3
Mahima Soota <67235999+mhmsoota@users.noreply.github.com>
2022-12-28 02:27:00 +0000

    Create start.py

---
+++ Day 8/start.py
@@ -0,0 +1,27 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+# def greet():
+#   print("Statement 1")
+#   print("Statement 2")
+#   print("Statement 3")
+
+# greet()
+
+
+# #Function that allows for input
+
+# def greet_with_name(name):
+#   print(f"Hello {name}")
+#   print(f"How do you do {name}")
+
+# greet_with_name("Mahima")
+
+#Functions with more than 1 input
+def greeting(name, location):
+  print(f"Hello {name}")
+  print(f"What is it like in {location}?")
+
+greeting("Mahima", "Delhi")
